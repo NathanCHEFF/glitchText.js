7e917ed8a553f5763b720d987b75532745542594
NathanCHEFF <54266398+NathanCHEFF@users.noreply.github.com>
2021-08-28 16:22:11 +0300

    Create glitchText.js

---
+++ glitchText.js
@@ -0,0 +1,38 @@
+(function(){
+    function glitchText(){
+
+    }
+
+    glitchText.prototype.showAnimate = function( Text ){
+        /* show animated text */
+    }
+
+    glitchText.prototype.getRandomRender = function(  ){
+        /* return random method */
+    }
+
+    glitchText.prototype.renderExploid = function(  ){
+        /* show render exploid text */
+    }
+
+    glitchText.prototype.changePosition = function(  ){
+        /* set new position */
+    }
+
+    glitchText.prototype.getRandomPos = function(  ){
+        /* return random position 4 div */
+    }
+
+    glitchText.prototype.showAnimate = function(  ){
+        /* show animated text */
+    }
+
+    glitchText.prototype.showAnimate = function( Text ){
+        /* show animated text */
+    }
+
+    glitchText.prototype.renderExpHor = function( Text ){
+        /* рендер деления по горизонтали со здвигом текста */
+    }
+
+    })();
